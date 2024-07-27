ce6e1e3a6d2d4d75e9e36257c7ea65c817112eff
stanislavermohin <ermohin1999@gmail.com>
2024-07-27 20:48:58 +0300

    add account creation and login placeholder logic in main.js

--- js/main.js
+++ js/main.js
@@ -21,3 +21,37 @@ const validateUsernameOnCreateAccount = async (username) => {
 const validatePasswordOnCreateAccount = async (password) => {
     return true;
 }
+
+const getUserAndPassword = async () => {
+    const form = document.getElementById("systemSignInForm");
+    const user = form.getElementById("emailOrPhone").value;
+    const password = form.getElementById("userPassword").value;
+
+    return user, password;
+}
+
+const onSignInFormData = async () => {
+    const [user, password] = await getUserAndPassword();
+    const valid = await validateUsernameOnSignIn(user) && await validatePasswordOnSignIn(user, password);
+
+    if (valid) {
+        return "Signed in " + user + " " + password;
+    }
+    else {
+        return "Not signed in";
+    };
+}
+
+const onCreateAccountFormData = async () => {
+    const [user, password] = await getUserAndPassword();
+    const valid = await validateUsernameOnCreateAccount(user) && await validatePasswordOnCreateAccount(password);
+
+    if (valid) {
+        console.log(user);
+        return "Created account " + user + " " + password;
+    }
+
+    else {
+        return "Account not created";
+    };
+}
