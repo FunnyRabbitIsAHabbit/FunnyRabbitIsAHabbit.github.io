cd03d0e4438839bb9cb6b5dd972955ff31d24518
stanislavermohin <ermohin1999@gmail.com>
2024-07-27 19:30:43 +0300

    add login validation placeholders in main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ const openForm = () => {
 const closeForm = () => {
     document.getElementById("myForm").style.display = "none";
 }
+
+const validateUsernameOnSignIn = async (username) => {
+    return true;
+}
+
+const validatePasswordOnSignIn = async (username, password) => {
+    return true;
+}
+
+const validateUsernameOnCreateAccount = async (username) => {
+    return true;
+}
+
+const validatePasswordOnCreateAccount = async (password) => {
+    return true;
+}
