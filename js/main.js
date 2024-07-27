e22563f51f071685584175a14ff29192d2e98f93
stanislavermohin <ermohin1999@gmail.com>
2024-07-27 21:11:59 +0300

    add remember user logic in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+const userRemembered = ". User is remembered.";
+
 const openForm = () => {
     document.getElementById("loginForm").style.display = "flex";
+    document.getElementById("mainScreen").style.display = "none";
 }
 
 const closeForm = () => {
     document.getElementById("loginForm").style.display = "none";
+    document.getElementById("mainScreen").style.display = "flex";
 }
 
 const validateUsernameOnSignIn = async (username) => {
@@ -25,16 +29,19 @@ const validatePasswordOnCreateAccount = async (password) => {
 const getUserAndPassword = async () => {
     const user = document.getElementById("emailOrPhone").value;
     const password = document.getElementById("userPassword").value;
+    const isRememberUser = document.getElementById("rememberUser").checked;
 
-    return [user, password];
+    return [user, password, isRememberUser];
 }
 
 const onSignInFormData = async () => {
-    const [user, password] = await getUserAndPassword();
+    const [user, password, isRememberUser] = await getUserAndPassword();
     const valid = await validateUsernameOnSignIn(user) && await validatePasswordOnSignIn(user, password);
 
     if (valid) {
-        return "Signed in " + user + " " + password;
+        let toReturn = "Signed in as @user=" + user + " with @pass-d=" + password;
+        if (isRememberUser) {toReturn += userRemembered}
+        return toReturn
     }
     else {
         return "Not signed in";
@@ -42,12 +49,14 @@ const onSignInFormData = async () => {
 }
 
 const onCreateAccountFormData = async () => {
-    const [user, password] = await getUserAndPassword();
+    const [user, password, isRememberUser] = await getUserAndPassword();
     const valid = await validateUsernameOnCreateAccount(user) && await validatePasswordOnCreateAccount(password);
 
+
     if (valid) {
-        console.log(user);
-        return "Created account " + user + " " + password;
+        let toReturn = "Created account @user=" + user + " with @pass-d=" + password
+        if (isRememberUser) {toReturn += userRemembered}
+        return toReturn;
     }
 
     else {
